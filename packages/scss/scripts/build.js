2cfe29e2024b1e9234ece770c663e731defb12b9
Max Plotitsyn <karrtopelka@Maxs-MacBook-Pro.local>
2023-08-17 02:36:03 +0300

    [ADD] build for css

---
+++ packages/scss/scripts/build.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const sass = require('sass');
+
+const scssSourcePath = path.resolve(__dirname, '../src');
+const normalizePath = path.resolve(__dirname, '../node_modules/normalize-scss/sass');
+
+const globalScssFilePath = path.resolve(__dirname, '../src/global.scss');
+const outputFilePath = path.resolve(__dirname, '../src/lib/global.css');
+
+const componentTypes = ['atoms', 'molecules', 'organisms'];
+
+const getAllComponents = () => {
+  let allComponents = [];
+
+  componentTypes.forEach((type) => {
+    const components = fs
+      .readdirSync(path.resolve(__dirname, `../src/${type}`))
+      .map((component) => ({
+        input: path.resolve(__dirname, `../src/${type}/${component}`),
+        output: path.resolve(__dirname, `../src/lib/${type}/${component.replace('.scss', '.css')}`),
+      }));
+
+    allComponents = [...allComponents, ...components];
+  });
+
+  return allComponents;
+};
+
+const compiler = (input, output) => {
+  const folder = output.split('/').slice(0, -1).join('/');
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder, { recursive: true });
+  }
+
+  const result = sass
+    .compile(input, {
+      // style: 'compressed',
+      loadPaths: [scssSourcePath, normalizePath],
+    })
+    .css.toString();
+
+  fs.writeFileSync(output, result);
+};
+
+compiler(globalScssFilePath, outputFilePath);
+
+getAllComponents().forEach((component) => {
+  compiler(component.input, component.output);
+});
